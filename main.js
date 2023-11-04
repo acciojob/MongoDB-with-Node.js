4e08d9a815a680413302accf876b6a932501ed3d
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 22:08:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-Implement the processData function in app.js:
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
-const fs = require('fs');
-const { Transform } = require('stream');
+const app = express();
+const port = process.env.PORT || 3000;
 
-function processData(inputFilePath, outputFilePath) {
-  // Implement this function
-}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-processData('input.txt', 'output.txt');
+const url = '<insert your MongoDB connection string here>';
+const dbName = '<insert your database name here>';
+const collectionName = '<insert your collection name here>';
 
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+
+  app.post('/resetpassword/:id', (req, res) => {
+    const userId = req.params.id;
+    const newPassword = req.body.password;
+
+    // TODO: Implement password reset functionality here
+
+    res.send('Password reset successful');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+});
