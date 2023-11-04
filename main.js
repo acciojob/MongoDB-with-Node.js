5a88697df20809378d11196c22e185fae5c75522
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 22:07:04 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+Implement the processData function in app.js:
+
+const fs = require('fs');
+const { Transform } = require('stream');
+
+function processData(inputFilePath, outputFilePath) {
+  // Implement this function
+}
+
+processData('input.txt', 'output.txt');
+
